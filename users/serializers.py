49448d5e4f502e15f4f8a047454783fc849d28a9
Lee Dawon <dawon337@gmail.com>
2023-03-19 15:29:13 +0900

    UPDATE user login app

--- users/serializers.py
+++ users/serializers.py
@@ -5,14 +5,13 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.validators import UniqueValidator
 
+from django.contrib.auth import authenticate
+
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
-        required = True,
-        validators = [
-            UniqueValidator(queryset=User.objects.all())
-        ],
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())],
     )
-
     password = serializers.CharField(
         write_only=True,
         required=True,
@@ -22,20 +21,41 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username' , 'password' , 'password2' , 'email')
+        fields = ('username', 'password', 'password2', 'email')
 
     def validate(self, data):
         if data['password'] != data['password2']:
             raise serializers.ValidationError(
-                {"password":"Password fields didn't match."}
-            )
+                {"password": "Password fields didn't match."})
+
+        email = data['email']
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError(
+                {"email": "Email address already exists."})
+
         return data
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username = validated_data['username'],
+            username=validated_data['username'],
+            email=validated_data['email'],
         )
+
         user.set_password(validated_data['password'])
         user.save()
-        token = Token.objects.create(user = user)
+        token = Token.objects.create(user=user)
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required = True)
+    password = serializers.CharField(required = True, write_only = True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user:
+            token = Token.objects.get(user=user)
+            return token
+        raise serializers.ValidationError(
+            {"error": "Unable to log in with provided credentials."}
+        )
+
